11f6e63028af3f49982859475b2e20d74006ba86
David Rosko <rosko.dav@gmail.com>
2022-12-22 18:01:19 +0100

    Add model for events

---
+++ APIGateway.Core/APIGateway.Core/Kafka/Messages/MluviiWebhook.cs
@@ -0,0 +1,11 @@
+﻿using APIGateway.Core.Controllers.Webhook;
+using Newtonsoft.Json.Linq;
+
+namespace APIGateway.Core.Kafka.Messages
+{
+    public class WebhookEvent
+    {
+        public string eventType { get; set; }
+        public JObject data { get; set; }
+    }
+}
