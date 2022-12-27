f766ecd1b4165457a126a2d5da9475b0dda3374c
David Rosko <rosko.dav@gmail.com>
2022-12-27 15:26:04 +0100

    Add message mluvii webhook

--- APIGateway.Core/APIGateway.Core/Kafka/Messages/MluviiWebhook.cs
+++ APIGateway.Core/APIGateway.Core/Kafka/Messages/MluviiWebhook.cs
@@ -1,11 +1,12 @@
-﻿using APIGateway.Core.Controllers.Webhook;
+﻿using System.IO;
+using APIGateway.Core.Controllers.Webhook;
 using Newtonsoft.Json.Linq;
 
 namespace APIGateway.Core.Kafka.Messages
 {
     public class WebhookEvent
     {
-        public string eventType { get; set; }
-        public JObject data { get; set; }
+        public string EventType { get; set; }
+        public string JsonData { get; set; }
     }
 }
