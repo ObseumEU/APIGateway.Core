18debb69492be788a7a47927efa8ee978d37cd42
David Rosko <rosko.dav@gmail.com>
2023-03-12 14:26:33 +0100

    Add polly support

---
+++ APIGateway.Core/APIGateway.Core/Polly/PollyPolicy.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Polly;
+using Polly.Retry;
+using System;
+
+namespace APIGateway.Core.Polly
+{
+    public static class PollyPolicy
+    {
+        public static AsyncRetryPolicy AutoRetryExponencial(int count = 13)
+        {
+            return Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(count, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (exception, retryCount, context) => Console.WriteLine($"try: {retryCount}, Exception: {exception.Message}"));
+        }
+    }
+}
