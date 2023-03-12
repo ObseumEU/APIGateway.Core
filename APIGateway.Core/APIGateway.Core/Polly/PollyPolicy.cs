b2e24e9ff819f0386184eaebe2094548ef13abd4
David Rosko <rosko.dav@gmail.com>
2023-03-12 15:44:44 +0100

    Add loging

--- APIGateway.Core/APIGateway.Core/Polly/PollyPolicy.cs
+++ APIGateway.Core/APIGateway.Core/Polly/PollyPolicy.cs
@@ -1,17 +1,30 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using Polly;
 using Polly.Retry;
 using System;
 
 namespace APIGateway.Core.Polly
 {
-    public static class PollyPolicy
+    public interface IPollyPolicy
     {
-        public static AsyncRetryPolicy AutoRetryExponencial(int count = 13)
+        AsyncRetryPolicy AutoRetryExponencial(int count = 13, string logRetryMessage = null);
+    }
+    
+    public class PollyPolicy : IPollyPolicy
+    {
+        private readonly ILogger<PollyPolicy> _log;
+        public PollyPolicy(ILogger<PollyPolicy> log)
+        {
+            _log = log;
+        }
+       
+
+        public AsyncRetryPolicy AutoRetryExponencial(int count = 13, string logRetryMessage = null)
         {
             return Policy
                 .Handle<Exception>()
-                .WaitAndRetryAsync(count, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (exception, retryCount, context) => Console.WriteLine($"try: {retryCount}, Exception: {exception.Message}"));
+                .WaitAndRetryAsync(count, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (exception, retryCount, context) => _log.LogError($"try: {retryCount}, Message: {logRetryMessage} Exception: {exception.Message}"));
         }
     }
 }
