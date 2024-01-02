20ad135ab1ddbb19b697aa41c6b974f0daec5e62
David Rosko <rosko.dav@gmail.com>
2024-01-02 21:54:09 +0100

    Revert "Move contract WebhookEvent to different library"
    This reverts commit 4e281289c5fa4d6465af3bbeae9bf0f94d01b60c.

---
+++ APIGateway.Core/APIGateway.Core/Kafka/Messages/MluviiWebhook.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using APIGateway.Core.Controllers.Webhook;
+using Newtonsoft.Json.Linq;
+
+namespace APIGateway.Core.Kafka.Messages
+{
+    public class WebhookEvent
+    {
+        public string EventType { get; set; }
+        public string JsonData { get; set; }
+    }
+}
